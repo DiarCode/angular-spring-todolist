379989904197cb63c8c88abaf1d28a5f72ed1b4e
Diar Begisbayev <diar_beg64@mail.ru>
2023-05-04 11:17:23 +0600

    All files is pushed to remote

---
+++ server/src/main/java/kz/diarcode/angularspringtodolist/AngularSpringTodolistApplication.java
@@ -0,0 +1,13 @@
+package kz.diarcode.angularspringtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringTodolistApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/kz/diarcode/angularspringtodolist/AngularSpringTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.diarcode.angularspringtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
