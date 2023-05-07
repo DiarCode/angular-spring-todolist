fe5cad41c17f9d342da9569d5310fad8b72a5045
Diar Begisbayev <diar_beg64@mail.ru>
2023-05-07 17:59:24 +0600

    environement added

--- client/src/app/shared/constants/api.url.ts
+++ client/src/app/shared/constants/api.url.ts
@@ -1,3 +1,3 @@
-import { env } from 'process';
+import { environment } from 'environment/environment.prod';
 
-export const API_URL = env.API_URL || 'http://localhost:8080/api/v1';
+export const API_URL = environment.API_URL || 'http://localhost:8080/api/v1';
---
+++ client/src/environment/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  API_URL: 'angular-spring-todolist-production.up.railway.app',
+};
