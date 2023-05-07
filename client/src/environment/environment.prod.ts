2bfcb89f7a625f29bcb5c96ccfc92e184bb88676
Diar Begisbayev <diar_beg64@mail.ru>
2023-05-07 18:21:08 +0600

    env api url changed

--- client/src/environment/environment.prod.ts
+++ client/src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  API_URL: 'angular-spring-todolist-production.up.railway.app/api/v1',
+  API_URL: 'https://angular-spring-todolist-production.up.railway.app/api/v1',
 };
